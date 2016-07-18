f2049b760e29e48a448cb37a27fcc60ec6f6608e
Liam Young <liam.young@canonical.com>
2016-07-18 08:08:10 +0000

    Support services which listen on multiple ports
    The APIConfigurationAdapter assumed that a service would only listen
    on one port but many OpenStack services will listen on multiple ports
    often seperating admin traffic from general traffic.

--- charms_openstack/adapters.py
+++ charms_openstack/adapters.py
@@ -436,11 +436,24 @@ class APIConfigurationAdapter(ConfigurationAdapter):
         service_ports = {}
         if self.port_map:
             for service in self.port_map.keys():
-                service_ports[service] = [
-                    self.port_map[service]['admin'],
-                    ch_cluster.determine_apache_port(
-                        self.port_map[service]['admin'],
-                        singlenode_mode=True)]
+                port_types = list(self.port_map[service].keys())
+                port_types.sort()
+                for port_type in self.port_map[service].keys():
+                    listen_port = self.port_map[service][port_type]
+                    key = '{}_{}'.format(service, port_type)
+                    used_ports = [service_ports[svc][0]
+                                  for svc in service_ports.keys()]
+                    if listen_port in used_ports:
+                        hookenv.log("Not adding haproxy listen stanza for {} "
+                                    "port is already in use".format(key),
+                                    level=hookenv.WARNING)
+                        continue
+                    service_ports[key] = [
+                        self.port_map[service][port_type],
+                        ch_cluster.determine_apache_port(
+                            self.port_map[service][port_type],
+                            singlenode_mode=True)]
+
         return service_ports
 
     @property
@@ -499,6 +512,10 @@ class APIConfigurationAdapter(ConfigurationAdapter):
                     'ip': ip,
                     'port': self.determine_service_port(
                         self.port_map[service]['admin'])}
+                for port_type in self.port_map[service].keys():
+                    port_key = '{}_port'.format(port_type)
+                    info[key][port_key] = self.determine_service_port(
+                        self.port_map[service][port_type])
                 info[key]['url'] = '{proto}://{ip}:{port}'.format(**info[key])
         return info
 
